9cc0c8e7ff87a8b5361673232b38b096459c6412
Shirima <kimarios@vodacom.co.tz>
2018-09-25 15:43:36 +0300

    Testing B2C transactions

--- app/Services/Payments/Mpesa/MpesaPaymentB2C.php
+++ app/Services/Payments/Mpesa/MpesaPaymentB2C.php
@@ -35,8 +35,8 @@ trait MpesaPaymentB2C
         curl_setopt($ch, CURLOPT_SSLCERT, '/var/www/html/storage/mpesa/tkj.vodacom.co.tz.cer');*/
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        /*curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);*/
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         $response = curl_exec($ch);
         //dd(curl_getinfo($ch, CURLINFO_HTTP_CODE));
         //Check HTTP status code
