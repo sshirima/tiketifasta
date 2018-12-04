b11b171fb91453db4a1c9567a5eecad73f2ea8ae
Shirima <kimarios@vodacom.co.tz>
2018-12-05 01:31:12 +0300

    Testing Tigopesa, Mpesa

--- resources/views/users/pages/bookings/booking_confirmation.blade.php
+++ resources/views/users/pages/bookings/booking_confirmation.blade.php
@@ -5,8 +5,6 @@
 @endsection
 
 @section('content')
-    @include('flash::message')
-    @include('includes.errors.message')
     <section class="features-icons bg-light text-center">
         <div class="container">
             @if(isset($booking))
