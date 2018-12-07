30f49e8a78073b3361e302e6f6a4b50b7b8f9281
Shirima <kimarios@vodacom.co.tz>
2018-12-07 10:16:28 +0300

    Testing Tigopesa, Mpesa

--- config/database.php
+++ config/database.php
@@ -45,16 +45,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            /*'host' => env('DB_HOST_PROD', '127.0.0.1'),
+            'host' => env('DB_HOST_PROD', '127.0.0.1'),
             'port' => env('DB_PORT_PROD', '3306'),
             'database' => env('DB_DATABASE_PROD', 'forge'),
             'username' => env('DB_USERNAME_PROD', 'forge'),
-            'password' => env('DB_PASSWORD_PROD', ''),*/
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'password' => env('DB_PASSWORD_PROD', ''),
+            /*'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', ''),*/
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
