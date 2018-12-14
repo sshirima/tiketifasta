fff0fd22d68663e55b385fc4e6c02a84c2d07022
Shirima <kimarios@vodacom.co.tz>
2018-12-14 16:21:51 +0300

    Mpesa, Tigopesa B2C intergration

--- app/Http/Controllers/Admins/TigoB2CController.php
+++ app/Http/Controllers/Admins/TigoB2CController.php
@@ -70,9 +70,7 @@ class TigoB2CController extends BaseController
             'language' => config('payments.tigo.bc2.language'),
         ]);
 
-        return json_encode($tigoB2C);
-
-        /*//$tigoB2C = $this->createTigoB2CModel($receiver,$input['amount']);
+        //$tigoB2C = $this->createTigoB2CModel($receiver,$input['amount']);
 
         //Generate and Send OTP
         $otp = rand(1000, 9999);
@@ -92,7 +90,7 @@ class TigoB2CController extends BaseController
         }else{
             return view('admins.pages.payments.tigoB2C_send_cash')->with(['otpIsSent'=>false, 'error'=>$sendSMS['error']]);
             //return view('admins.pages.sms.send_sms')->with(['sentStatus'=>false,'error'=>$sendSMS['error']]);
-        }*/
+        }
     }
 
     /**
