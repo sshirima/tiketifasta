4b4a54e6b76d73eea5e15dc4fa433428543788ec
Shirima <kimarios@vodacom.co.tz>
2018-12-15 23:04:51 +0300

    Mpesa, Tigopesa B2C intergration

--- resources/views/admins/pages/payments/mpesa/mpesaB2C_send_cash.blade.php
+++ resources/views/admins/pages/payments/mpesa/mpesaB2C_send_cash.blade.php
@@ -64,7 +64,7 @@
                                 <div class="container col-md-11 col-md-offset-1">
                                     <div class="alert alert-success"> <i class="fas fa-check-circle"></i> Transfer has been posted...</div>
                                     <div class="alert alert-default">
-                                        {{$response->response->transactionID}}
+                                        {{$response}}
                                     </div>
                                 </div>
                             </div>
