28929fd5186df1f69356b91f154ddd87ee000506
Shirima <kimarios@vodacom.co.tz>
2018-12-15 22:27:47 +0300

    Mpesa, Tigopesa B2C intergration

--- resources/views/admins/pages/payments/mpesa/mpesaB2C_send_cash.blade.php
+++ resources/views/admins/pages/payments/mpesa/mpesaB2C_send_cash.blade.php
@@ -94,7 +94,32 @@
                     <div class="row">
                         <div class="container col-md-6 col-md-offset-1">
                             <form class="form-horizontal" role="form" method="get" action="{{route('admin.mpesab2c.send_cash.submit')}}" accept-charset="UTF-8" style="padding: 20px">
-                                @include('admins.pages.payments.mpesa.mpesab2c_send_cash_fields')
+                                
+                                <div class="form-group">
+                                    {!! Form::label('operator', __('admin_pages.page_payment_method_fields_label_name'), ['class'=>'col-sm-5 control-label', 'for'=>'operator']) !!}
+                                    <div class="col-sm-7">
+                                        {{Form::select('payment_mode',[0=>'Select payment method','mpesa'=>'M-PESA','tigopesa'=>'TIGO-PESA'],null,['class'=>'form-control'])}}
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('receiver', 'Receiver', ['class'=>'col-sm-5 control-label', 'for'=>'receiver']) !!}
+                                    <div class="col-sm-7">
+                                        {!! Form::number('receiver', old('receiver'), ['class' => 'form-control', 'placeholder'=>'Receiver']) !!}
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('amount', 'Amount', ['class'=>'col-sm-5 control-label', 'for'=>'amount']) !!}
+                                    <div class="col-sm-7">
+                                        {!! Form::number('amount', old('amount'), ['class' => 'form-control', 'placeholder'=>'Amount']) !!}
+                                    </div>
+                                </div>
+                                @csrf
+                                <div class="form-group">
+                                    <div class="col-sm-7 col-md-offset-5">
+                                        {!! Form::submit(__('admin_pages.page_payment_account_fields_issue'), ['class' => 'btn btn-primary']) !!}
+                                        <a href="{!! route('admin.tigob2c.send_cash') !!}" class="btn btn-default">{{__('admin_pages.page_locations_fields_button_cancel')}}</a>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
