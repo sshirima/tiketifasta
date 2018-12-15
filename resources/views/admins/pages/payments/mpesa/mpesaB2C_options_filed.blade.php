82e32a6dfc725e05b1ece43ff22b200ef03e2d91
Shirima <kimarios@vodacom.co.tz>
2018-12-15 22:21:19 +0300

    Mpesa, Tigopesa B2C intergration

--- resources/views/admins/pages/payments/mpesa/mpesaB2C_options_filed.blade.php
+++ resources/views/admins/pages/payments/mpesa/mpesaB2C_options_filed.blade.php
@@ -1,1 +1,2 @@
 <li class="{{Request::is('admin/merchants-payments/mpesa') ? 'active' : ''}}"><a href="{{route('admin.mpesab2c.index')}}" >View transactions</a></li>
+<li class="{{Request::is('admin/merchant-payments/send_cash*') ? 'active' : ''}}"><a href="{{route('admin.mpesab2c.send_cash')}}" >Issue transaction</a></li>
